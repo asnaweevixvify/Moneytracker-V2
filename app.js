afb33a4bdd9a99fef932a9eaeca483d8b4761735
asnaweevixvify <asnaweeezor@gmail.com>
2025-04-30 11:51:00 +0700

    system

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
+const btnOne = document.getElementById('bgonecall');
+const btnTwo = document.getElementById('bgtwocall');
+const btnThree = document.getElementById('bgthreecall');
+const btnFour = document.getElementById('bgfourcall');
+const bgOne = document.getElementById('bg1');
+const bgTwo = document.getElementById('bg2');
+const bgThree = document.getElementById('bg3');
+const bgFour = document.getElementById('bg4');
+const topicText = document.getElementById('topictext');
+const addNewBtn = document.getElementById('addnewbtn');
+const btnAdd = document.getElementById('btnadd');
+let selected = document.getElementById('select');
+let dateType = document.getElementById('datetype');
+let nameType = document.getElementById('nametype');
+let moneyType = document.getElementById('moneytext');
+let catType = document.getElementById('cattype');
+let earnList = document.getElementById('earnlist');
+let payList = document.getElementById('paylist');
+let earnMoneyTotalEl = document.getElementById('earntotal');
+let payMoneyTotalEl = document.getElementById('paytotal');
+
+let earnMoneyTotal = 0;
+let payMoneyTotal = 0;
+let currentMoneyEarn = 0;
+let currentMoneyPay = 0;
+let earnArr = [];
+let payArr = [];
+let countPay = 0;
+let countEarn = 0;
+let listAddEarn;
+let listAddPay;
+let myChart;
 const ctx = document.getElementById('myChart');
 
-  new Chart(ctx, {
+function updateChart(){
+  if (myChart) {
+    myChart.destroy(); 
+  }
+
+  myChart = new Chart(ctx, {
     type: 'doughnut',
     data: {
       labels: ['รายรับ', 'รายจ่าย'],
       datasets: [{
         label: 'จำนวนเงิน (บาท)',
-        data: [],
+        data: [earnMoneyTotal,payMoneyTotal],
         backgroundColor: [
-            'green',   // รายรับ = สีเขียว
-            'red'      // รายจ่าย = สีแดง
-          ],
+          'green',   
+          'red'      
+        ],
         borderWidth: 1
       }]
     },
@@ -22,138 +59,138 @@ const ctx = document.getElementById('myChart');
       }
     }
   });
-
-const btnOne = document.getElementById('bgonecall')
-const btnTwo = document.getElementById('bgtwocall')
-const btnThree = document.getElementById('bgthreecall')
-const btnFour = document.getElementById('bgfourcall')
-const bgOne = document.getElementById('bg1')
-const bgTwo = document.getElementById('bg2')
-const bgThree = document.getElementById('bg3')
-const bgFour = document.getElementById('bg4')
-const topicText = document.getElementById('topictext')
-const addNewBtn = document.getElementById('addnewbtn')
-const btnAdd = document.getElementById('btnadd')
-let selected = document.getElementById('select')
-let dateType = document.getElementById('datetype')
-let nameType = document.getElementById('nametype')
-let moneyType = document.getElementById('moneytext')
-let catType = document.getElementById('cattype')
-let earnList = document.getElementById('earnlist')
-let payList = document.getElementById('paylist')
-let earnMoneyTotalEl = document.getElementById('earntotal')
-let payMoneyTotalEl = document.getElementById('paytotal')
-let earnMoneyTotal=0
-let payMoneyTotal=0
-let earnArr = []
-let payArr = []
-let countPay = 0
-let countEarn = 0;
-
-
-btnOne.addEventListener('click',callBgOne)
-btnTwo.addEventListener('click',callBgTwo)
-btnThree.addEventListener('click',callBgThree)
-btnFour.addEventListener('click',callBgFour)
-addNewBtn.addEventListener('click',callBgFour)
-
-function callBgOne(){
-    bgOne.style.display='flex'
-    bgTwo.style.display='none'
-    bgThree.style.display='none'
-    bgFour.style.display='none'
-    topicText.innerText='บันทึกรายรับรายจ่าย'
 }
 
-function callBgTwo(){
-    bgOne.style.display='none'
-    bgTwo.style.display='flex'
-    bgThree.style.display='none'
-    bgFour.style.display='none'
-    topicText.innerText='รายการรายรับ'
+btnOne.addEventListener('click', callBgOne);
+btnTwo.addEventListener('click', callBgTwo);
+btnThree.addEventListener('click', callBgThree);
+btnFour.addEventListener('click', callBgFour);
+addNewBtn.addEventListener('click', callBgFour);
+btnAdd.addEventListener('click', addToHistory);
+
+function callBgOne() {
+  bgOne.style.display = 'flex';
+  bgTwo.style.display = 'none';
+  bgThree.style.display = 'none';
+  bgFour.style.display = 'none';
+  topicText.innerText = 'บันทึกรายรับรายจ่าย';
 }
 
-function callBgThree(){
-    bgOne.style.display='none'
-    bgTwo.style.display='none'
-    bgThree.style.display='flex'
-    bgFour.style.display='none'
-    topicText.innerText='รายการรายจ่าย'
+function callBgTwo() {
+  bgOne.style.display = 'none';
+  bgTwo.style.display = 'flex';
+  bgThree.style.display = 'none';
+  bgFour.style.display = 'none';
+  topicText.innerText = 'รายการรายรับ';
 }
 
-function callBgFour(){
-    bgOne.style.display='none'
-    bgTwo.style.display='none'
-    bgThree.style.display='none'
-    bgFour.style.display='flex'
-    topicText.innerText='เพิ่มรายการ'
+function callBgThree() {
+  bgOne.style.display = 'none';
+  bgTwo.style.display = 'none';
+  bgThree.style.display = 'flex';
+  bgFour.style.display = 'none';
+  topicText.innerText = 'รายการรายจ่าย';
 }
 
-btnAdd.addEventListener('click',addToHistory)
+function callBgFour() {
+  bgOne.style.display = 'none';
+  bgTwo.style.display = 'none';
+  bgThree.style.display = 'none';
+  bgFour.style.display = 'flex';
+  topicText.innerText = 'เพิ่มรายการ';
+}
 
-function addToHistory(){
+function addToHistory() {
   Swal.fire({
-    title:  `<h3>เพิ่มรายการสำเร็จ<h3>`,
+    title: `<h3>เพิ่มรายการสำเร็จ<h3>`,
     icon: "success",
     draggable: true
   });
-  if(selected.value === 'earn'){
+  if(selected.value === 'none' || dateType.value === '' || nameType.value === '' || moneyType.value === '' || catType.value==='none'){
+    Swal.fire({
+      icon: "error",
+      title: `<h3>โปรดกรอกข้อมูลให้ครบ<h3>`,
+    });
+  }
+  else if (selected.value === 'earn') {
     earnArr.push({
-      date:dateType.value,
-      name:nameType.value,
-      money:moneyType.value,
-      cat:catType.value})
-      let listAddEarn = document.createElement('ul')
-      listAddEarn.classList.add('itemlist')
-      listAddEarn.id='earnlist'
-      listAddEarn.innerHTML=`
-            <li class="list">${earnArr[countEarn].date}</li>
-            <li class="list">${earnArr[countEarn].name}</li>
-            <li class="list">${earnArr[countEarn].money}</li>
-            <li class="list">${earnArr[countEarn].cat}</li>
-      `
-      if(earnMoneyTotalEl.innerText==='0฿'){
-        earnMoneyTotal = moneyType.value
-      }
-      else{
-        
-      }
-      earnMoneyTotalEl.innerHTML=`${earnMoneyTotal}฿`
-      bgTwo.appendChild(listAddEarn)
-      countEarn++
-      callBgTwo()
-  }
-  else{
+      date: dateType.value,
+      name: nameType.value,
+      money: moneyType.value,
+      cat: catType.value
+    });
+    showListEarn();
+    calEarn();
+    resetDisplay();
+  } else {
     payArr.push({
-      date:dateType.value,
-      name:nameType.value,
-      money:moneyType.value,
-      cat:catType.value})
-      let listAddPay = document.createElement('ul')
-      listAddPay.classList.add('itemlist')
-      listAddPay.id='paylist'
-      listAddPay.innerHTML=`
-            <li class="list">${payArr[countPay].date}</li>
-            <li class="list">${payArr[countPay].name}</li>
-            <li class="list">${payArr[countPay].money}</li>
-            <li class="list">${payArr[countPay].cat}</li>
-      `
-      if(payMoneyTotalEl.innerText==='0฿'){
-        payMoneyTotal = moneyType.value
-      }
-      else{
-        
-      }
-      payMoneyTotalEl.innerHTML=`${payMoneyTotal}฿`
-      bgThree.appendChild(listAddPay)
-      countPay++
-      callBgThree()
-      
+      date: dateType.value,
+      name: nameType.value,
+      money: moneyType.value,
+      cat: catType.value
+    });
+    showListPay();
+    calPay();
+    resetDisplay();
   }
-  selected.value = 'none'
-  dateType.value = ''
-  nameType.value = ''
-  moneyType.value = ''
-  catType.value = 'none'
 }
 
+function showListEarn() {
+  listAddEarn = document.createElement('ul');
+  listAddEarn.classList.add('itemlist');
+  listAddEarn.id = 'earnlist';
+  listAddEarn.innerHTML = `
+    <li class="list">${earnArr[countEarn].date}</li>
+    <li class="list">${earnArr[countEarn].name}</li>
+    <li class="list">${earnArr[countEarn].money}</li>
+    <li class="list">${earnArr[countEarn].cat}</li>
+    <li class="list"><i class="fa-solid fa-trash " style="color: #ff0000;"></i></li>
+  `;
+  bgTwo.appendChild(listAddEarn);
+  countEarn++;
+  callBgTwo();
+}
+
+function showListPay() {
+  listAddPay = document.createElement('ul');
+  listAddPay.classList.add('itemlist');
+  listAddPay.id = 'paylist';
+  listAddPay.innerHTML = `
+    <li class="list">${payArr[countPay].date}</li>
+    <li class="list">${payArr[countPay].name}</li>
+    <li class="list">${payArr[countPay].money}</li>
+    <li class="list">${payArr[countPay].cat}</li>
+    <li class="list"><i class="fa-solid fa-trash" style="color: #ff0000;"></i></li>
+  `;
+  bgThree.appendChild(listAddPay);
+  countPay++;
+  callBgThree();
+}
+
+function resetDisplay() {
+  selected.value = 'none';
+  dateType.value = '';
+  nameType.value = '';
+  moneyType.value = '';
+  catType.value = 'none';
+}
+
+function calEarn() {
+  if (earnMoneyTotalEl.innerText === '0฿') {
+    earnMoneyTotal = parseInt(moneyType.value);
+  } else {
+    earnMoneyTotal += parseInt(moneyType.value);
+  }
+  earnMoneyTotalEl.innerHTML = `${Intl.NumberFormat().format(earnMoneyTotal)}฿`;
+  updateChart()
+}
+
+function calPay() {
+  if (payMoneyTotalEl.innerText === '0฿') {
+    payMoneyTotal = parseInt(moneyType.value);
+  } else {
+    payMoneyTotal += parseInt(moneyType.value);
+  }
+  payMoneyTotalEl.innerHTML = `${Intl.NumberFormat().format(payMoneyTotal)}฿`;
+  updateChart()
+}
